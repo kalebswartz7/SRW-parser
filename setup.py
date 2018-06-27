b6010566e970c89e053baeac8124284944c41212
Kaleb Swartz <kalebswartz7@gmail.com>
2018-06-27 14:28:08 -0400

    Updating dependencies

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kalebswartz7/parse-and-visualize',
-    version='0.0.2',
+    version='0.0.4',
     zip_safe=False,
 )
