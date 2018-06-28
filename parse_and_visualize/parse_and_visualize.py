5555f7e715bc3dbc5decc994f0f6cefb959593ca
Kaleb Swartz <kalebswartz7@gmail.com>
2018-06-28 15:54:53 -0400

    Added console input options

--- parse_and_visualize/parse_and_visualize.py
+++ parse_and_visualize/parse_and_visualize.py
@@ -1,7 +1,7 @@
-# Parse the data from the dat file -> Points vs Horizontal Position = how many rows per column in the 2D array 
-#                                  -> Points vs Vertical Position = how many columns in the 2D array 
+# Parse the data from the dat file -> Points vs Horizontal Position = how many rows per column in the 2D array
+#                                  -> Points vs Vertical Position = how many columns in the 2D array
 
-#       Create a dictionary and populate it with other dictionaries based on the data in the dat file 
+#       Create a dictionary and populate it with other dictionaries based on the data in the dat file
 #       Use this data to add to the graph
 
 import numpy as np
@@ -16,7 +16,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def get_parameters(d):
     """
-    Read in first 10 lines of input file to get parameters for data 
+    Read in first 10 lines of input file to get parameters for data
 
     Parameters
     ----------
@@ -87,7 +87,7 @@ def get_parameters(d):
 def populate_dictionary(line, d_to_populate, starting_point,
     ending_point, d, d1=None, type1='float'):
     """
-    Populate dictionary based on wanting a certain portion of a string 
+    Populate dictionary based on wanting a certain portion of a string
 
     Parameters
     ----------
@@ -100,7 +100,7 @@ def populate_dictionary(line, d_to_populate, starting_point,
     ending_point : string
         string character that comes right after 'substring'
     d : dictionary
-        second level dictionary to store 'substring' 
+        second level dictionary to store 'substring'
     d1 : dictionary
         third level dictionary to store 'substring', optional
     type1 : string
@@ -117,7 +117,7 @@ def populate_dictionary(line, d_to_populate, starting_point,
     else:
         substring_value = float(line[start:end])
 
-    #Checks to see if d1 is actually necessary 
+    #Checks to see if d1 is actually necessary
     if not d1:
         d_to_populate[d] = substring_value
     else:
@@ -147,13 +147,13 @@ def populate_matrix_smart(matrix, horizontal, vertical):
 
     Parameters
     ----------
-    matrix : 2D array 
+    matrix : 2D array
         array that will be populated by the data parsed in the input file
-    horizontal : int 
+    horizontal : int
         # of horizontal rows in matrix
     vertical : int
         # of vertical rows in matrix
-    
+
     """
     d = np.loadtxt('res_int_pr_se.dat')
     d2 = d.reshape((vertical, horizontal))
@@ -162,8 +162,25 @@ def populate_matrix_smart(matrix, horizontal, vertical):
 
 
 if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Plot image based on input file')
+    parser.add_argument('-d', '--dat_file', dest='dat_file', default='',
+                        help ='input .dat file')
+    parser.add_argument('-vp', '--vert_pos', dest='vert_pos', default='',
+                        help='input vertical slice position', type=str)
+    parser.add_argument('-hp', '--horiz_pos', dest='horiz_pos', default='',
+                        help='input horizontal slice position', type=str)
+    parser.add_argument('-vl', '--vert_label', dest='vert_label', default='',
+                        help='input vertical label string', type=str)
+    parser.add_argument('-hl', '--horiz_label', dest='horiz_label', default='',
+                        help='input horizontal label string', type=str)
+    parser.add_argument('-e', '--extent_labels', dest='extent_labels', default=None,
+                        help='input extent labels for image ranges', nargs='+', type=int)
+    args = parser.parse_args()
+
     initial_data = {'photon': {}, 'horizontal': {}, 'vertical': {},}
-    matrix = [[]] 
+    matrix = [[]]
 
     get_parameters(initial_data)
     horizontal = initial_data['horizontal']['points']
@@ -177,17 +194,46 @@ if __name__ == '__main__':
                                     ['initial_position'], 'micro')
     vertical_final = _unit_change(initial_data['vertical']
                                   ['final_position'], 'micro')
+    if args.dat_file:
+        img = np.loadtxt(args.dat_file)
+        img = img.reshape((vertical, horizontal))
+        fig = plt.figure()
+        kwargs = {'fig': fig}
 
-    matrix = populate_matrix_smart(matrix, horizontal, vertical)
-    fig = plt.figure(figsize=(8, 10))
-    title = f'After Propagation (E={initial_data["photon"]["initial_energy"]} {initial_data["photon"]["units"]})'
-    axis_data = [initial_data['horizontal']['axis_label'] + " [µm]",
-                 initial_data['vertical']['axis_label'] + " [µm]",
-                 "Intensity " + initial_data['intensity']]
-    cs = CrossSection(fig, horiz_loc= 'top', vert_loc='right', title=title,
-                     vert_label=axis_data[1], horiz_label=axis_data[0],
-                     extent_labels=[horizontal_initial, horizontal_final, vertical_initial, vertical_final])
+        if args.vert_pos:
+            if args.vert_pos == 'right':
+                args.vert_pos = 'right'
+            kwargs['vert_loc'] = args.vert_pos
 
-    cs.update_image(matrix)
-    plt.show()
+        if args.horiz_pos:
+            if args.horiz_pos == 'bottom':
+                args.horiz_pos = 'bottom'
+            kwargs['horiz_loc'] = args.horiz_pos
+
+        if args.vert_label:
+            kwargs['vert_label'] = args.vert_label
+
+        if args.horiz_label:
+            kwargs['horiz_label'] = args.horiz_label
+
+        if args.extent_labels:
+            kwargs['extent_labels'] = args.extent_labels
+
+        cs = CrossSection(**kwargs)
+        cs.update_image(img)
+        plt.show()
+    else:
+
+        matrix = populate_matrix_smart(matrix, horizontal, vertical)
+        fig = plt.figure(figsize=(8, 10))
+        title = f'After Propagation (E={initial_data["photon"]["initial_energy"]} {initial_data["photon"]["units"]})'
+        axis_data = [initial_data['horizontal']['axis_label'] + " [µm]",
+                     initial_data['vertical']['axis_label'] + " [µm]",
+                     "Intensity " + initial_data['intensity']]
+        cs = CrossSection(fig, horiz_loc= 'top', vert_loc='right', title=title,
+                         vert_label=axis_data[1], horiz_label=axis_data[0],
+                         extent_labels=[horizontal_initial, horizontal_final, vertical_initial, vertical_final])
+
+        cs.update_image(matrix)
+        plt.show()
 
